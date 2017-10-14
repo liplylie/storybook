336a668a9c7f09d6d64a50138cd489197c045424
Jaafar Skafi <jaafarskafi1@gmail.com>
2017-10-14 00:56:36 -0700

    change directory that serves static files

--- server/index.py
+++ server/index.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from config import BaseConfig
 from flask_bcrypt import Bcrypt
 
-app = Flask(__name__, static_folder="./static/dist", template_folder="./static")
+# app = Flask(__name__, static_folder="./static/dist", template_folder="./static")
+app = Flask(__name__, static_folder="../client/public", template_folder="../client/public")
 app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
