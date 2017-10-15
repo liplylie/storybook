dcf60faa412244cff951b435c01928f1711d4eb8
Jaafar Skafi <jaafarskafi1@gmail.com>
2017-10-14 19:55:49 -0700

    correct import path

--- server/manage.py
+++ server/manage.py
@@ -1,7 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from application.app import app, db
+from server.application.app import app, db
 
 migrate = Migrate(app, db)
 manager = Manager(app)
