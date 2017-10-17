6b2089da4bddb60d565c9dcd769d586699d43bee
Angie Tang <atang167@gmail.com>
2017-10-16 19:32:42 -0700

    Add tab navigator.

---
+++ mobile/navigator/tabNavigator.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { Image, Button } from 'react-native'
+import { TabNavigator, NavigationActions } from 'react-navigation'
+
+class MyHomeScreen extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'Home',
+    // Note: By default the icon is only shown on iOS. Search the showIcon option below.
+    tabBarIcon: ({ tintColor }) => (
+      <Image
+        source={require('./chats-icon.png')}
+        style={[styles.icon, {tintColor: tintColor}]}
+      />
+    ),
+  };
+
+  render() {
+    return (
+      <Button
+        onPress={}
+        title=""
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 26,
+    height: 26,
+  },
+});
+
+const MyApp = TabNavigator({
+  Home: {
+    screen: MyHomeScreen,
+  },
+  Notifications: {
+    screen: MyNotificationsScreen,
+  },
+}, {
+  tabBarPosition: 'top',
+  animationEnabled: true,
+  tabBarOptions: {
+    activeTintColor: '#e91e63',
+  },
+});
