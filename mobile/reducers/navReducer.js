872d772443b315c65f2795ab2c41e39517a86e9d
Angie Tang <atang167@gmail.com>
2017-10-17 12:57:50 -0700

    Add tab structure to app.

--- mobile/reducers/navReducer.js
+++ mobile/reducers/navReducer.js
@@ -1,34 +1,39 @@
 import { NavigationActions } from 'react-navigation'
-import { StackNavigator } from '../navigator/stackNavigator'
+import AppNavigator from '../components/appNavigation'
 
-const navReducer = (state=initialNavState, action) => {
-  let nextState;
+// const initialNavState = AppNavigator.router.getStateForAction(
+//   AppNavigator.router.getActionForPathAndParams('Main')
+// );
 
-  switch(action.type) {
-    case 'Login': {
-      nextState = AppNavigator.router.getStateForAction(
-        NavigationActions.back(),
-        state
-      );
-      break;
-    }
-    case 'Logout': {
-      nextState = AppNavigator.router.getStateForAction(
-        NavigationActions.navigate({ routeName: 'Login' }),
-        state
-      );
-      break;
-    }
-    default: {
-      nextState = AppNavigator.router.getStateForAction(
-        action,
-        state
-      );
-      break;
-    }
-  }
 
-  return nextState || state;
-};
+// const navReducer = (state=initialNavState, action) => {
+//   let nextState;
 
-export default navReducer;
+//   switch(action.type) {
+//     case 'Login': {
+//       nextState = AppNavigator.router.getStateForAction(
+//         NavigationActions.back(),
+//         state
+//       );
+//       break;
+//     }
+//     case 'Logout': {
+//       nextState = AppNavigator.router.getStateForAction(
+//         NavigationActions.navigate({ routeName: 'Login' }),
+//         state
+//       );
+//       break;
+//     }
+//     default: {
+//       nextState = AppNavigator.router.getStateForAction(
+//         action,
+//         state
+//       );
+//       break;
+//     }
+//   }
+
+//   return nextState || state;
+// };
+
+// export default navReducer;
