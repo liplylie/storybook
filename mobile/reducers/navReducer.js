76df7def3b5eade83dac18e7372f4297b8d6c472
Angie Tang <atang167@gmail.com>
2017-10-16 19:32:42 -0700

    Add tab navigator.

---
+++ mobile/reducers/navReducer.js
@@ -0,0 +1,34 @@
+import { NavigationActions } from 'react-navigation'
+import { StackNavigator } from '../navigator/stackNavigator'
+
+const navReducer = (state=initialNavState, action) => {
+  let nextState;
+
+  switch(action.type) {
+    case 'Login': {
+      nextState = AppNavigator.router.getStateForAction(
+        NavigationActions.back(),
+        state
+      );
+      break;
+    }
+    case 'Logout': {
+      nextState = AppNavigator.router.getStateForAction(
+        NavigationActions.navigate({ routeName: 'Login' }),
+        state
+      );
+      break;
+    }
+    default: {
+      nextState = AppNavigator.router.getStateForAction(
+        action,
+        state
+      );
+      break;
+    }
+  }
+
+  return nextState || state;
+};
+
+export default navReducer;
--- mobile/reducers/reducer.js
+++ mobile/reducers/reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
+import Navigator from './navReducer'
 
 const Reducer = combineReducers({
-
+  Navigator
 });
 
 export default Reducer; 
