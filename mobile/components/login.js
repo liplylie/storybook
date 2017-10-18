c84ef07736d03e01c93442eeaf9527045f1887fa
Daniel Chong <danchong625@gmail.com>
2017-10-17 18:12:15 -0700

    [Update] Implement working fb login + aws

--- mobile/components/login.js
+++ mobile/components/login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Image, StyleSheet, Text } from 'react-native';
-import { LoginButton } from 'react-native-fbsdk';
+import { View, Image, StyleSheet, Text, Button } from 'react-native';
+import { LoginButton, AccessToken, GraphRequest, GraphRequestManager } from 'react-native-fbsdk';
+import AWS, { Config, CognitoIdentityCredentials } from 'aws-sdk';
 
 class Login extends Component {
   constructor(props) {
@@ -8,22 +9,54 @@ class Login extends Component {
   }
 
   render() {
-    <View>
-      <LoginButton
-        publishPermissions={["publish_actions"]}
-        onLoginFinished={
-          (error, result) => {
-            if (error) {
-              alert("Login failed with error: " + result.error);
-            } else if (result.isCancelled) {
-              alert("Login was cancelled");
-            } else {
-              alert("Login was successful with permissions: " + result.grantedPermissions)
+    return (
+      <View>
+        <LoginButton
+          readPermissions={["email","public_profile"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("Login failed with error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("Login was cancelled");
+              } else {
+                AccessToken.getCurrentAccessToken()
+                  .then(data => {
+                    const { accessToken, userID } = data;
+                    let req = new GraphRequest('/me', {
+                      httpMethod: 'GET',
+                      version: 'v2.5',
+                      parameters: {
+                        'fields': {
+                            'string' : 'name,picture,email'
+                        }
+                      }
+                      }, (err, res) => {
+                          console.log('results: ', res);
+                          err ? console.log('error: ', err) : null;
+                      }); 
+                    new GraphRequestManager().addRequest(req).start();
+                    AWS.config.region = "us-west-2";
+                    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                      IdentityPoolId: 'us-west-2:ce49614a-87c5-48e2-95c9-343d38075481',
+                      Logins: {
+                        'graph.facebook.com': accessToken
+                      }
+                    });
+                  })
+                  .then(() => {
+                    AWS.config.credentials.get(function() {
+                      console.log('aws session token: ', AWS.config.credentials.sessionToken);
+                      console.log('aws access key Id: ', AWS.config.credentials.accessKeyId);
+                    })
+                  })
+              }
             }
           }
-        }
-        onLogoutFinished={() => alert("User logged out")}/>
-    </View>
+          onLogoutFinished={() => alert("User logged out")}
+        />
+      </View>
+    )
   }
 }
 
