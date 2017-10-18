79eeec8390a18e647a68914a8e4522daafab14b0
Jaafar Skafi <jaafarskafi1@gmail.com>
2017-10-18 16:29:34 -0700

    change database to elephantsql

--- server/config.py
+++ server/config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
 
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    SQLALCHEMY_DATABASE_URI = 'postgres://zdhbeaoe:x_h7PkNBTqrc5CeucKQuTgByEq-D8lb8@pellefant.db.elephantsql.com:5432/zdhbeaoe'
 
 class DevelopmentConfig(Config):
 
