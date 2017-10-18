99741281c60f655a247770c7f4d2f3048e57295d
Jaafar Skafi <jaafarskafi1@gmail.com>
2017-10-17 20:10:11 -0700

    add user, image, and tags tables to schema

--- server/app/schema.py
+++ server/app/schema.py
@@ -1,15 +1,46 @@
-from flask import Flask, g
-from flask_assets import Environment, Bundle
 from flask_sqlalchemy import SQLAlchemy
-from config import app_config
 import sqlalchemy_utils
 import sqlite3
 from app import db
 
 class Image(db.Model):
+  __tablename__ = 'Image'
   id = db.Column(db.Integer, primary_key=True)
-  url = db.Column(db.String(80))
-  tags = db.Column(db.String(120))
+  image_url = db.Column(db.String(120))
+  scn_code = db.Column(db.String(120))
+  user_id = db.Column(db.Integer)
+  location = db.Column(db.String(120))
+  likes_count = db.Column(db.Integer)
+  caption = db.Column(db.Integer)
+  image_tags_array = db.Column(db.String(120))
+
+  def __init__(self, image_url, scn_code, user_id, location, likes_count, caption, image_tags_array):
+    self.image_url = image_url
+    self.scn_code = scn_code
+    self.user_id = user_id
+    self.location = location
+    self.likes_count = likes_count
+    self.caption = caption
+    self.image_tags_array = image_tags_array
 
   def __repr__(self):
-    return '<Image: %r>' % self.url + ' ' + '<Tags: %r>' % self.tags
+    return '<Image tags: %r>' % self.image_tags_array + ' ' + '<image url: %r>' % self.image_url
+
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(120))
+  friends_count = db.Column(db.Integer)
+  uer_tags_array = db.Column(db.String(120))
+
+  def __repr__(self):
+    return '<User: %r>' % self.name + ' ' + '<fuser riends count: %r>' % self.friends_count + ' ' + '<user tags: %r>' % self.tags_array
+
+
+class Tags(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  url = db.Column(db.String(120))
+  tags_array = db.Column(db.String(120))
+
+  def __repr__(self):
+    return '<Tags: %r>' % self.url + ' ' + '<Tags: %r>' % self.tags_array
