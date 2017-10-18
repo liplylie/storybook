0e1217fdc1054ab22b0325b7acf864222fdfe57c
Angie Tang <atang167@gmail.com>
2017-10-17 19:53:39 -0700

    Change styling for views.

--- mobile/components/profileTab/views/Profile.js
+++ mobile/components/profileTab/views/Profile.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native'
 
 const Profile = (props) => {
   return (
-    <View>
+    <View style={{flex: 1, justifyContent: 'center'}}>
       <Text>This is profile view</Text>
     </View> 
   )
