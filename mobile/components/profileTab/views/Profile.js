06228a13d121a49c1b304ba048f11adb985bd798
Jordan Daniels <decentpianist@gmail.com>
2017-10-18 16:26:01 -0700

    photo and name from fb renders on profile view

--- mobile/components/profileTab/views/Profile.js
+++ mobile/components/profileTab/views/Profile.js
@@ -1,13 +1,57 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
 
 
-const Profile = (props) => {
+const Profile = ( props ) => {
+	console.log(props, 'profile props')
+	let name = '';
+	let picture = '';
+	if (props.profileInfo.name){
+		console.log('success')
+		name = props.profileInfo.name;
+		picture = props.profileInfo.picture.data.url
+	} 
   return (
     <View style={{flex: 1, justifyContent: 'center'}}>
       <Text>This is profile view</Text>
+    <View>
+    	<Image source={{uri:picture}} style={styles.profilePicture}/>
+      <Text style={styles.profileName}>{name}</Text>
     </View> 
   )
 }
 
-export default Profile;
+const mapStateToProps = (store) =>{
+  console.log(store, 'profile js state')
+ 
+  return {
+    profileInfo: store.Profile
+  }
+}
+
+const styles = StyleSheet.create({
+	profileName: {
+		textAlign: 'center',
+		padding: 10
+	},
+	profilePicture: {
+		width: 60, 
+		height: 60,
+		justifyContent: 'center',
+		padding: 10,
+		borderRadius: 10
+	}
+})
+
+// const mapDispatchToProps = (dispatch) =>{
+//   return {
+//     addToDo: (todo) =>{
+//       dispatch(addTodo(todo))
+//     }
+//   };
+// }
+// {profileInfo.Profile.profileInfo.name}
+// src={{uri:profileInfo.Profile.profileInfo.picture.data.url}}
+
+export default connect(mapStateToProps, null)(Profile);
