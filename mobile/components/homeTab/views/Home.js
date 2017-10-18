c77b413f6ba75cc2ff4647eb8bc95a04538388bf
Daniel Chong <danchong625@gmail.com>
2017-10-17 19:48:32 -0700

    [Update] Update home view to reflect login

--- mobile/components/homeTab/views/Home.js
+++ mobile/components/homeTab/views/Home.js
@@ -1,57 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import LoginView from '../../login';
+import SignupView from '../../signup.js';
 
-export default class App extends Component<{}> {
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const {} = this.props;
+
+    let showLogin = true;
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Image 
+          style={styles.image}
+          resizeMethod='resize'
+          resizeMode='contain'
+          source={require('../../../logo.jpg')} 
+        />
+        {showLogin ? <LoginView /> : <SignupView />}
       </View>
     );
   }
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'white',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  image: {
+    width: 400,
+    height: 200
+  }
 });
