580d5a05fa7c076765989a92dafbb8b24e263a9f
Jordan Daniels <decentpianist@gmail.com>
2017-10-18 16:26:01 -0700

    photo and name from fb renders on profile view

--- mobile/reducers/profileReducer.js
+++ mobile/reducers/profileReducer.js
@@ -1,7 +1,14 @@
 import { ProfileNavigator } from '../components/profileTab/navigationConfig'
 import { NavigationActions } from 'react-navigation';
 
+const initialState = {
+	profileInfo: []
+}
 const ProfileReducer = (state, action) => {
+	console.log(state, 'profile Reducer')
+	if (action.type === 'PROFILE_INFO'){
+		return Object.assign({}, state, action.payload )
+	}
   return ProfileNavigator.router.getStateForAction(action, state);
 }
 
