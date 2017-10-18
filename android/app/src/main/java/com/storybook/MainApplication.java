69abb4893691f8015cecee46e1963c8766478553
Daniel Chong <danchong625@gmail.com>
2017-10-17 18:10:20 -0700

    [Edit] Edit to correctly call fbsdk

--- android/app/src/main/java/com/storybook/MainApplication.java
+++ android/app/src/main/java/com/storybook/MainApplication.java
@@ -35,7 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(),
+            // new FBSDKPackage(),
           new FBSDKPackage(mCallbackManager)
       );
     }
