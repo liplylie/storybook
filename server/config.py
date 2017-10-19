65aaa1e2e2b4c0301549ee14db4ed550a1c190aa
Angie Tang <atang167@gmail.com>
2017-10-19 09:50:52 -0700

    Add Python and pip.

--- server/config.py
+++ server/config.py
@@ -1,8 +1,20 @@
+<<<<<<< HEAD
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     """general app-wide settings"""
+=======
+# Put in any global settings for the server
+
+class Config(object):
+    """
+    Common configurations
+    """
+
+    # Put any configurations here that are common across all environments
+    # Declare which mode will be used when using app
+>>>>>>> Add Python and pip.
 
 class DevelopmentConfig(Config):
 
