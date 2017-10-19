cccff6f460f3b23c34a490068d9568105297dd50
Daniel Chong <danchong625@gmail.com>
2017-10-19 09:37:10 -0700

    [Add] Implement s3 connection

--- mobile/components/auth/Login.js
+++ mobile/components/auth/Login.js
@@ -70,6 +70,19 @@ class Login extends Component {
                       console.log('aws access key Id: ', AWS.config.credentials.accessKeyId);
                     })
                   })
+                  .then(() => {
+                    let s3 = new AWS.S3({
+                      params: {Bucket: 'storybooknativeapp'}
+                    })
+
+                    s3.listObjects({Delimiter: '/'}, function(err, data) {
+                      if(err) {
+                        console.log('s3 error: ', err);
+                      } else {
+                        console.log('s3 data: ', data);
+                      }
+                    })
+                  })
               }
             }
           }
