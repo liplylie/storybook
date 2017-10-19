25b68be463d4b22948e8c1594517a9a76b1c3a79
Daniel Chong <danchong625@gmail.com>
2017-10-19 15:10:37 -0700

    [Update] Refactor to redux

--- mobile/components/auth/Login.js
+++ mobile/components/auth/Login.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Image, StyleSheet, Text, Button } from 'react-native';
+import { View, Image, StyleSheet, Text, Button, AlertIOS } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { LoginButton } from 'react-native-fbsdk';
 
 import * as AuthActions from '../../actions/authActions.js';
 
@@ -11,26 +12,17 @@ class Login extends Component {
   }
 
   componentDidMount() {
-    AccessToken.getCurrentAccessToken()
-    .then(data => {
-      const { accessToken, userID } = data;
-      AWS.config.region = "us-west-2";
-      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: 'us-west-2:ce49614a-87c5-48e2-95c9-343d38075481',
-        Logins: {
-          'graph.facebook.com': accessToken
-        }
-      });
-    })
-    .then(() => {
-      AWS.config.credentials.get(function() {
-        console.log('aws session token: ', AWS.config.credentials.sessionToken);
-        console.log('aws access key Id: ', AWS.config.credentials.accessKeyId);
-      })
-    })
+    const { actions, authorized } = this.props;
+
+    if(!authorized) {
+      actions.getFBToken();
+      actions.getUserInfo();
+    }
   }
 
   render() {
+    const { authorized, actions }  = this.props;
+
     return (
       <View>
         <LoginButton
@@ -38,57 +30,29 @@ class Login extends Component {
           onLoginFinished={
             (error, result) => {
               if (error) {
-                alert("Login failed with error: " + result.error);
+                console.log('Error logging in to FB: ', error);
+                AlertIOS.alert(
+                  '',
+                  'Error Logging Into Facebook'
+                )
               } else if (result.isCancelled) {
-                alert("Login was cancelled");
+                AlertIOS.alert(
+                  '',
+                  'Log in was cancelled'
+                )
               } else {
-                AccessToken.getCurrentAccessToken()
-                  .then(data => {
-                    const { accessToken, userID } = data;
-                    let req = new GraphRequest('/me', {
-                      httpMethod: 'GET',
-                      version: 'v2.5',
-                      parameters: {
-                        'fields': {
-                            'string' : 'name,picture,email'
-                        }
-                      }
-                      }, (err, res) => {
-                          console.log('results: ', res);
-                          err ? console.log('error: ', err) : null;
-                      }); 
-                    new GraphRequestManager().addRequest(req).start();
-                    AWS.config.region = "us-west-2";
-                    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-                      IdentityPoolId: 'us-west-2:ce49614a-87c5-48e2-95c9-343d38075481',
-                      Logins: {
-                        'graph.facebook.com': accessToken
-                      }
-                    });
-                  })
-                  .then(() => {
-                    AWS.config.credentials.get(function() {
-                      console.log('aws session token: ', AWS.config.credentials.sessionToken);
-                      console.log('aws access key Id: ', AWS.config.credentials.accessKeyId);
-                    })
-                  })
-                  .then(() => {
-                    let s3 = new AWS.S3({
-                      params: {Bucket: 'storybooknativeapp'}
-                    })
-
-                    s3.listObjects({Delimiter: '/'}, function(err, data) {
-                      if(err) {
-                        console.log('s3 error: ', err);
-                      } else {
-                        console.log('s3 data: ', data);
-                      }
-                    })
-                  })
+                actions.getFBToken();
+                actions.getUserInfo();
               }
             }
           }
-          onLogoutFinished={() => alert("User logged out")}
+          onLogoutFinished={() => {
+            actions.logoutUser();
+            AlertIOS.alert(
+              '',
+              "User logged out"
+            )}
+          }
         />
       </View>
     )
