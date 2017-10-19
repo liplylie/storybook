beb2ea2922f81ab37a475a5dc139708ea945c82c
Jordan Daniels <decentpianist@gmail.com>
2017-10-19 10:46:06 -0700

    preparing to add map to userProfile

---
+++ mobile/components/homeTab/views/MapView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+const MapView = (props) => {
+  return (
+    <View>
+      <Text>This is map view</Text>
+    </View> 
+  )
+}
+
+export default MapView;
