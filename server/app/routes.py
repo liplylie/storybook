254ee1818c194c4cd85dff527c3034862c5975fb
Jaafar Skafi <jaafarskafi1@gmail.com>
2017-10-19 21:50:53 -0700

    add sampler route

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,8 +1,12 @@
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects import postgresql
+from flask import request
+from werkzeug.datastructures import ImmutableMultiDict
 import sqlalchemy_utils
+import json
 # from app.app import db
+# from schema import Image, User, Relationship, Chatroom, Messages, Comments, Likes
 from app import app
 # from schema import Image, User, Relationship, Chatroom, Messages, Comments, Likes
 
@@ -11,6 +15,9 @@ def index():
   """This module renders main page"""
   return render_template('index.html')
 
-@app.route('/api/images')
-def test():
-  print("ghosts and stuff")
+@app.route('/api/images', methods=['GET','POST'])
+
+def fetchImages():
+  print('HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA')
+  data = dict(request.args)
+  print(data)
