419284e87910e26081417b3760d56b5b0cf85b83
Jaafar Skafi <jaafarskafi1@gmail.com>
2017-10-19 20:08:40 -0700

    define relationship between chatroom and users

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,12 +1,16 @@
 from flask import render_template
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects import postgresql
+import sqlalchemy_utils
+# from app.app import db
 from app import app
+# from schema import Image, User, Relationship, Chatroom, Messages, Comments, Likes
 
 @app.route('/')
 def index():
   """This module renders main page"""
   return render_template('index.html')
 
-@app.route('/test')
+@app.route('/api/images')
 def test():
   print("ghosts and stuff")
