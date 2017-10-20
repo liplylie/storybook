4e717ed92d24773796fed941867a183d90d0ab7c
Daniel Chong <danchong625@gmail.com>
2017-10-19 20:41:35 -0700

    [Add] Import react-native-camera library to android project

--- android/app/src/main/java/com/storybook/MainApplication.java
+++ android/app/src/main/java/com/storybook/MainApplication.java
@@ -3,6 +3,7 @@ package com.storybook;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTCameraPackage(),
             // new FBSDKPackage(),
           new FBSDKPackage(mCallbackManager)
       );
