e4d4d19a01d9a16b13bf63d633d80733eec1b93d
Daniel Chong <danchong625@gmail.com>
2017-10-20 19:39:24 -0700

    [Refactor] Refactor view to cameraView

--- mobile/components/cameraTab/views/Camera.js
+++ mobile/components/cameraTab/views/Camera.js
@@ -1,56 +1,22 @@
 import React from 'react';
 import { View, Text, StyleSheet, Dimensions, TouchableHighlight } from 'react-native';
-import Camera from 'react-native-camera';
 
-class CameraView extends React.Component {
+import CameraView from './CameraView.js';
+
+class Camera extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  takePicture() {
-    const options = {};
-    //options.location = ...
-    this.camera.capture({metadata: options})
-      .then((data) => console.log(data))
-      .catch(err => console.error(err));
+  static navigationOptions = {
+    header: null,
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Camera
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-          style={styles.preview}
-          aspect={Camera.constants.Aspect.fill}
-          captureQuality={Camera.constants.CaptureQuality["720"]}
-        >
-        <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
-        </Camera>
-      </View> 
+      <CameraView navigation={this.props.navigation}/>
     )
   }
 }
 
-export default CameraView;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    color: '#000',
-    padding: 10,
-    margin: 40
-  }
-});
+export default Camera;
