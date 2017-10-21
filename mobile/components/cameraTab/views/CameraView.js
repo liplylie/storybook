01303612302d5182da00f2098aa0b11e038c3995
Daniel Chong <danchong625@gmail.com>
2017-10-20 19:39:01 -0700

    [Init] Initialize Camera/Image picker view

---
+++ mobile/components/cameraTab/views/CameraView.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions, TouchableHighlight, Button } from 'react-native';
+import ImagePicker from 'react-native-image-crop-picker';
+
+class CameraView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <View>
+        <Button
+          title="click to add picture"
+          onPress={() => {
+            ImagePicker.openPicker({
+              width: 720,
+              height: 1280,
+              cropping: true
+            })
+              .then(image => {
+                console.log('image: ', image, image.path);
+                this.props.navigation.navigate('Post');
+              })
+              .catch(err => {
+                console.log('image picker error: ', err);
+              })
+          }}
+        />
+        <Button
+          title="click to take picture"
+          onPress={() => {
+            ImagePicker.openCamera({
+              width: 720,
+              height: 1280,
+              cropping: true
+            })
+              .then(image => {
+                console.log('image: ', image, image.path);
+                this.props.navigation.navigate('Post');
+              })
+              .catch(err => {
+                console.log('capture picker error: ', err);
+              })
+          }}
+        />
+      </View>
+    )
+  }
+}
+
+export default CameraView;
