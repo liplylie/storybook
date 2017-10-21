5b726be60dc06057ade3fbbb78eea89235e5ee0a
Daniel Chong <danchong625@gmail.com>
2017-10-21 08:59:30 -0700

    [Update] Implement redux onto post view

--- mobile/components/cameraTab/views/Post.js
+++ mobile/components/cameraTab/views/Post.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import { View, Text } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-const Post = (props) => {
-  return (
-    <View>
-      <Text>This is post view</Text>
-    </View> 
-  )
+import * as CameraActions from '../../../actions/cameraActions';
+
+class Post extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput />
+        <TextInput />
+        <TextInput />
+        <Button />
+      </View>
+    )
+  }
 }
 
-export default Post;
+const postState = (state) => {
+  return {
+    image: state.CameraPost.image,
+  }
+}
+
+const postDispatch = (dispatch) => {
+  return {
+    actions: bindActionCreators(CameraActions, dispatch),
+  }
+}
+
+export default connect(postState, postDispatch)(Post);
