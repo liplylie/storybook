b32c156effe08d38cfcadc7ad6a5629f15e4b7fd
Daniel Chong <danchong625@gmail.com>
2017-10-20 19:38:18 -0700

    [Update] Update android java files to implement camera

--- android/app/src/main/java/com/storybook/MainApplication.java
+++ android/app/src/main/java/com/storybook/MainApplication.java
@@ -3,6 +3,7 @@ package com.storybook;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
@@ -36,7 +37,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RCTCameraPackage(),
+          new PickerPackage(),
+          new RCTCameraPackage(),
             // new FBSDKPackage(),
           new FBSDKPackage(mCallbackManager)
       );
