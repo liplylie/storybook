2a855dc478b635044333a121d4d224d0a7962d9e
Jordan Daniels <decentpianist@gmail.com>
2017-10-21 17:29:01 -0700

    fixed merge conflicts with camera

--- mobile/components/profileTab/navigationConfig.js
+++ mobile/components/profileTab/navigationConfig.js
@@ -2,23 +2,17 @@ import { StackNavigator } from 'react-navigation'
 import Profile from './views/Profile'
 import Friends from './views/Friends'
 import Messages from './views/Messages'
-<<<<<<< HEAD
 import Chat from './views/Chat'
-=======
 import UserMap from './views/map.js'
 import PicturesFromMarker from './views/picturesFromMarker'
->>>>>>> clicking on marker renders a new page
 
 const routeConfig = {
   Profile: { screen: Profile},
   Friends: { screen: Friends },
   Messages: { screen: Messages },
-<<<<<<< HEAD
   Chat: { screen: Chat },
-=======
   UserMap: { screen: UserMap },
   PicturesFromMarker: { screen: PicturesFromMarker}
->>>>>>> clicking on marker renders a new page
 }
 // going to disable the header for now
 const stackNavigatorConfig = {
