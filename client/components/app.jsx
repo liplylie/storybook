8c261a6235324cf72ec42ea0757c7314bc1bb202
Daniel Chong <danchong625@gmail.com>
2017-10-23 17:41:03 -0700

    [Update] Update to remove test methods and initialize views

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,43 +1,17 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-// const Promise = require('bluebird'); promisify later on...
+
+import Nav from './nav.jsx';
+import Home from './home.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      urlList: ["https://i.imgur.com/hy0rDYi.jpg"]
-    }
-    this.grabUrl = this.grabUrl.bind(this);
-    this.sendUrls = this.sendUrls.bind(this);
-  }
-
-  grabUrl(e) {
-    let separatedLinks = [];
-    separatedLinks = (e.target.value).split(/[,| +]+?|[\n]/g);
-    this.setState({urlList: separatedLinks});
-    console.log("hello world", this.state.urlList);
-  }
-
-  sendUrls() {
-    console.log(this.state.urlList);
-    this.state.urlList.forEach((link) => {
-      console.log('LIJK', link);
-      if (link !== "") {
-        axios({
-          method: 'post',
-          url: "/api/addurl",
-          data: link,
-        }).then(res => {}).catch(err => console.log(err));
-      };
-    })
   }
 
   render() {
     return (
       <div className="main">
-        <input type="text" placeholder="text" onChange={this.grabUrl}/>
-        <button onClick={this.sendUrls}>send urls</button>
+        <Nav />
       </div>
     )
   }
