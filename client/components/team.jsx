0bfb665e1928abb337d90fb6aa194826b3211989
Daniel Chong <danchong625@gmail.com>
2017-10-24 14:04:39 -0700

    [Update] Implement basic team view

--- client/components/team.jsx
+++ client/components/team.jsx
@@ -8,7 +8,56 @@ class Team extends Component {
 
   render() {
     return (
-      <div>TEAM</div>
+      <div style={{marginTop: 53}}>
+        <div className="row" style={{backgroundColor: 'lightcoral', height: '100vh', width: '100vw'}}>
+          <div className="col align-self-center">
+            <div style={{textAlign: 'center', margin: 'auto'}}>
+              <h1>Meet the Team</h1>
+              <p>TEAM SLOGAN AND WHATEVER GOES HERE</p>
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center" style={{height: '100vh'}}>
+          <div className="col align-self-center" style={{textAlign: 'right'}}>
+            <img src="http://via.placeholder.com/300x500" alt=""/>
+          </div>
+          <div className="col" style={{maxWidth: 200}}></div>
+          <div className="col align-self-center" style={{textAlign: 'left'}}>
+            <h1>Name 1</h1>
+            <p>Description</p>
+          </div>
+        </div>
+        <div className="row justify-content-center" style={{height: '100vh', backgroundColor: 'lightcyan'}}>
+          <div className="col align-self-center" style={{textAlign: 'right'}}>
+            <h1>Name 2</h1>
+            <p>Description</p>
+          </div>
+          <div className="col" style={{maxWidth: 200}}></div>
+          <div className="col align-self-center" style={{textAlign: 'left'}}>
+            <img src="http://via.placeholder.com/300x500" alt=""/>
+          </div>
+        </div>
+        <div className="row justify-content-center" style={{height: '100vh'}}>
+          <div className="col align-self-center" style={{textAlign: 'right'}}>
+            <img src="http://via.placeholder.com/300x500" alt=""/>
+          </div>
+          <div className="col" style={{maxWidth: 200}}></div>
+          <div className="col align-self-center" style={{textAlign: 'left'}}>
+            <h1>Name 3</h1>
+            <p>Description</p>
+          </div>
+        </div>
+        <div className="row justify-content-center" style={{height: '100vh', backgroundColor: 'lightcyan'}}>
+          <div className="col align-self-center" style={{textAlign: 'right'}}>
+            <h1>Name 4</h1>
+            <p>Description</p>
+          </div>
+          <div className="col" style={{maxWidth: 200}}></div>
+          <div className="col align-self-center" style={{textAlign: 'left'}}>
+            <img src="http://via.placeholder.com/300x500" alt=""/>
+          </div>
+        </div>
+      </div>
     )
   }
 }
