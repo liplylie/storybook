b07c8338932a893b907ad7a331b0e2860ba124f9
Daniel Chong <danchong625@gmail.com>
2017-10-24 11:12:29 -0700

    [Init] Initialize Info page

---
+++ client/components/info.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Info extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div> HI </div>
+    )
+  }
+}
+
+export default Info;
