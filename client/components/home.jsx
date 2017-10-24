3e4c84097859c57430cbc7373bd75db21fcce063
Daniel Chong <danchong625@gmail.com>
2017-10-23 19:29:30 -0700

    [Update] Update home view with placeholder img and grid layout

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -7,7 +7,26 @@ class Home extends Component {
   }
 
   render() {
-
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col" style={{textAlign: 'right'}}>
+            <img src="http://via.placeholder.com/300x500" />
+          </div>
+          <div className="col align-self-center" style={{textAlign: 'center'}}>
+            <h1>Download now</h1>
+            <div className="row justify-content-center">
+              <button type="button" className="btn btn-outline-dark">
+                App Store
+              </button>
+              <button type="button" className="btn btn-outline-dark">
+                Play Store
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
