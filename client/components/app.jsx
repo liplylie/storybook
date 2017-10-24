66040b574f3aa8f2cc4c3b9fbb28e2696b5882a4
Daniel Chong <danchong625@gmail.com>
2017-10-24 11:11:29 -0700

    [Add] Implement Router

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Nav from './nav.jsx';
 import Home from './home.jsx';
 import Footer from './footer.jsx';
+import Info from './info.jsx';
 
 class App extends Component {
   constructor(props) {
@@ -11,11 +13,14 @@ class App extends Component {
 
   render() {
     return (
-      <div className="main">
-        <Nav />
-        <Home />
-        <Footer />
-      </div>
+      <Router>
+        <div className="main">
+          <Nav />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/info" component={Info} />
+          <Footer />
+        </div>
+      </Router>
     )
   }
 }
