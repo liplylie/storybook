a7d149467aefbc8bd6b1797734d85902d3f2d904
Daniel Chong <danchong625@gmail.com>
2017-10-25 13:37:16 -0700

    [Update] Update to save image as a buffer

--- mobile/actions/cameraActions.js
+++ mobile/actions/cameraActions.js
@@ -1,4 +1,5 @@
 import AWS from 'aws-sdk';
+import { Buffer } from 'buffer';
 
 export const saveImage = (image) => {
   return function(dispatch) {
@@ -6,24 +7,26 @@ export const saveImage = (image) => {
   }
 }
 
-export const postImage = (obj, creds) => {
+export const postImage = (obj) => {
   return function(dispatch) {
     let s3 = new AWS.S3({
       params: {Bucket: 'storybooknativeapp'}
     })
   
-    let albumPhotosKey = encodeURIComponent('pictures') + '/' + creds.accessKey + '/';
+    let albumPhotosKey = encodeURIComponent('pictures') + '/' + obj.userId + '/';
     let photoKey = albumPhotosKey + obj.name;
     
     s3.upload({
-      Key: photoKey,
-      Body: obj.image.data,
+      Key: photoKey + '.jpg',
+      Body: new Buffer(obj.image.data, 'base64'),
       ACL: 'public-read',
       ContentType: obj.image.mime
     }, (err, data) => {
       err ? 
         console.log('Error uploading photo to AWS: ', err)
         : console.log('Successfully uploaded photo: ', data);
+
+        //Send img url (data.Location) to database
     })
   }
 }
