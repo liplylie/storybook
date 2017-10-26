825a0d263be92f6e209f3cbdc343549b807112bc
Angie Tang <atang167@gmail.com>
2017-10-26 16:16:43 -0700

    Fix message entry view to stateless component.

--- mobile/components/profileTab/views/Messages/MessageEntry.js
+++ mobile/components/profileTab/views/Messages/MessageEntry.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 import { View, Text, TouchableWithoutFeedback } from 'react-native'
 
-class MessageEntry extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+
+const MessageEntry = (props) => {
+  const {navigate} = this.props.navigate;
+  return (
+    <TouchableWithoutFeedback onPress={()=> navigate('Chat')}>
+      <Text>{this.props.friend}</Text>
+      <Text>{this.props.message}</Text>
+      <Image source={this.props.img}/>
+    </TouchableWithoutFeedback> 
+  )
+}
+
+// class MessageEntry extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
       // name: '',
       // img: ''
-    }
-  }
-  componentDidMount() {
+  //   }
+  // }
+  // componentDidMount() {
     // axios.get('api/friend/' + this.props.friendId)
     // .then(({ data }) => {
     //   this.setState({
@@ -20,16 +32,16 @@ class MessageEntry extends Component {
     // .catch(err => {
     //   console.log('Unable to get friend info', err);
     // })
-  }
+//   }
 
-  render() {
-    const {navigate} = this.props.navigation;
-    return (
-      <TouchableWithoutFeedback onPress={()=> navigate('Chat')}>
-        <Text>{this.props.name}</Text>
-        <Text>{this.props.message}</Text>
-        <Image source={this.props.img}/>
-      </TouchableWithoutFeedback> 
-    )
-  }
-}
+//   render() {
+//     const {navigate} = this.props.navigation;
+//     return (
+//       <TouchableWithoutFeedback onPress={()=> navigate('Chat')}>
+//         <Text>{this.props.friend}</Text>
+//         <Text>{this.props.message}</Text>
+//         <Image source={this.props.img}/>
+//       </TouchableWithoutFeedback> 
+//     )
+//   }
+// }
