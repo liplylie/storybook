e58380aadbf7395db35c95d26185d5d6b163beb3
Daniel Chong <danchong625@gmail.com>
2017-10-26 16:42:51 -0700

    [Update] Update for backend heroku server

--- react_server/server.js
+++ react_server/server.js
@@ -18,11 +18,11 @@ const io = socketIo(server);
 app.use(parser.json())
 app.use(parser.urlencoded({extended: true}))
 // app.use('/api', router)
-app.use(express.static(path.resolve(__dirname, '../client/public')))
+// app.use(express.static(path.resolve(__dirname, '../client/public')))
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
-})
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
+// })
 
 io.on('connection', socket => {
   socket.on('message', message => {
