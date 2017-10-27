c7ba9be4f37c341a107d98d498efb8a565af8d20
Daniel Chong <danchong625@gmail.com>
2017-10-26 19:57:16 -0700

    [Edit] Test for correct post

--- react_server/controllers/roomController.js
+++ react_server/controllers/roomController.js
@@ -1,4 +1,5 @@
-const db = require('../db/config')
+const db = require('../db/config');
+const Chatroom = require('../db/models/chatroom');
 
 module.exports = {
   getRooms: (req, res) => {
@@ -26,13 +27,60 @@ module.exports = {
       chatroom_recipient: req.body.friendId,
       admin: req.body.username
     })
-    .spread((room, created) => {
-      res.send(room);
+    .then(data => {
+      res.status(201).send(data);
     })
     .catch(err => {
-      res.status(500).send(err);
+      res.status(400).send(err)
     })
   },
+  // createRoom: (req, res) => {
+  //   //find where admin = userId and user = friendId OR admin = friendId and userId
+  //   //using req.body.userId, req.params.friendId 
+  //     //return roomId
+  //   //else create room
+  //     //return roomId
+  //   db.Chatroom.findAll({
+  //     where: { [Op.or]: [
+  //       {
+  //         chatroom_sender: req.body.userId, 
+  //         chatroom_recipient: req.body.friendId
+  //       },
+  //       {
+  //         chatroom_sender: req.body.friendId, 
+  //         chatroom_recipient: req.body.userId
+  //       },
+  //     ]},
+  //     include: [{
+  //       model: User,
+  //       attributes: [name, profile_image_url],
+  //     }]
+  //   })
+  //   .then(data => {
+  //     if (data.length) {
+  //       res.send(data);
+  //     } else {
+  //       db.Chatroom.create({
+  //         chatroom_sender: req.body.userId,
+  //         chatroom_recipient: req.params.friendId,
+  //       }, 
+  //       {
+  //         include: [{
+  //           model: User,
+  //           attributes: [name, profile_image_url],
+  //         }]
+  //       })
+  //       .then(data => {
+  //         res.send(data);
+  //       })
+  //       .catch(err => {
+  //         res.status(500).send(err); 
+  //       })
+  //     }
+  //   })
+  //   .catch(err => {
+  //     res.status(500).send(err);
+  //   })
   getPreview: (req, res) => {
     //get an object using req.params.roomId with admin, user, most recent message, time created at
     db.Messages.findAll({
