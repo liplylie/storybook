0b72285ebd4a6e89781bb88047a4dead9125b3ad
Daniel Chong <danchong625@gmail.com>
2017-10-27 11:17:18 -0700

    [Update] Update changes to create room

--- react_server/controllers/roomController.js
+++ react_server/controllers/roomController.js
@@ -31,56 +31,9 @@ module.exports = {
       res.status(201).send(data);
     })
     .catch(err => {
-      res.status(400).send(err)
+      res.status(400).send(err);
     })
   },
-  // createRoom: (req, res) => {
-  //   //find where admin = userId and user = friendId OR admin = friendId and userId
-  //   //using req.body.userId, req.params.friendId 
-  //     //return roomId
-  //   //else create room
-  //     //return roomId
-  //   db.Chatroom.findAll({
-  //     where: { [Op.or]: [
-  //       {
-  //         chatroom_sender: req.body.userId, 
-  //         chatroom_recipient: req.body.friendId
-  //       },
-  //       {
-  //         chatroom_sender: req.body.friendId, 
-  //         chatroom_recipient: req.body.userId
-  //       },
-  //     ]},
-  //     include: [{
-  //       model: User,
-  //       attributes: [name, profile_image_url],
-  //     }]
-  //   })
-  //   .then(data => {
-  //     if (data.length) {
-  //       res.send(data);
-  //     } else {
-  //       db.Chatroom.create({
-  //         chatroom_sender: req.body.userId,
-  //         chatroom_recipient: req.params.friendId,
-  //       }, 
-  //       {
-  //         include: [{
-  //           model: User,
-  //           attributes: [name, profile_image_url],
-  //         }]
-  //       })
-  //       .then(data => {
-  //         res.send(data);
-  //       })
-  //       .catch(err => {
-  //         res.status(500).send(err); 
-  //       })
-  //     }
-  //   })
-  //   .catch(err => {
-  //     res.status(500).send(err);
-  //   })
   getPreview: (req, res) => {
     //get an object using req.params.roomId with admin, user, most recent message, time created at
     db.Messages.findAll({
