6e82736c5c6ab506b355e99fd6943a998e6fac4a
Daniel Chong <danchong625@gmail.com>
2017-10-27 12:08:42 -0700

    [Init] Initialize User model

---
+++ react_server/db/models/user.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const db = require('../config');
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  profile_image_url: {
+    type: Sequelize.STRING
+  },
+  friends_count: {
+    type: Sequelize.INTEGER
+  },
+  user_tags_array: {
+    type: Sequelize.ARRAY({type: Sequelize.STRING})
+  }
+}, {
+  timestamps: false
+});
+
+User.sync();
+
+module.exports = User;
