d7129a9db8149a5c512ab947375704e40fbdb779
Daniel Chong <danchong625@gmail.com>
2017-10-28 10:51:24 -0700

    [Add] Add test User post

--- react_server/router.js
+++ react_server/router.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 const roomController = require('./controllers/roomController')
 const friendController = require('./controllers/friendController')
 
+const User = require('./db/models/user')
+
 router.get('/chat/:userId', roomController.getRooms);
 router.get('/chats/:roomId', roomController.getPreview);
 
@@ -20,5 +22,19 @@ router.post('deleteRequest', friendController.deleteRequest)
 router.post('/blockUser', friendController.blockUser)
 
 router.get('/search/:firstName/:lastName', friendController.search)
+
+router.post('/addUser', (req, res) => {
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    profile_image_url: req.body.image,
+  })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.send(err);
+    })
+})
 
 module.exports = router; 
