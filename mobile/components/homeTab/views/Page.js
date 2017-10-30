8e9b2ef284913d6d2d8fa8f071785a771b7df653
Jordan Daniels <decentpianist@gmail.com>
2017-10-30 12:33:30 -0700

    page backbone done need to add add comment feature

--- mobile/components/homeTab/views/Page.js
+++ mobile/components/homeTab/views/Page.js
@@ -1,28 +1,112 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, ScrollView, Dimensions } from 'react-native'
 import axios from 'axios'
+import { connect } from 'react-redux'
 //import { connect } from 'react-redux'
 
+const { width, height } =  Dimensions.get('window')
+
 class Page extends Component{
+	constructor(props){
+		super(props)
+		console.log(props, 'page props')
+	}
 	componentDidMount(){
 	// send get request for all images from clicked user
 
 	}
 	render(){
-	  return (
-	    <View style={{justifyContent: 'center', alignItems: 'center'}}>
-	      <Text>This is page view</Text>
-	    </View> 
+		let img1 = "https://timedotcom.files.wordpress.com/2014/08/t100_tv_spongebob_free1.jpg?quality=85"
+	  	 return (
+          <View style={styles.container}>
+            <View style={styles.title}> 
+              <Image 
+                style={styles.logo}
+                resizeMethod='resize'
+                resizeMode='contain'
+                source={require('../../../logo.jpg')} 
+              />
+            </View>
+				    <ScrollView>
+				    	<View>
+				    		<View style={styles.profileInfo}>
+				    			<Image style={styles.profilePicture} source={{uri:img1}} />
+				    			<Text> SpongeBob </Text>
+				    		</View>
+				      	<Image style={styles.image} source={{uri:img1}}/>
+				      	<Text style={styles.caption}> My Life is Great </Text>
+						  	<Text style={styles.likes}> Likes: 0 </Text>
+						  	<View>
+						  		<Text style={styles.userCommentName}> SquidWard <Text style={styles.userComment}> I hate you SpongeBob</Text></Text>
+						  		
+						  	</View>
+				      </View>
+				  	</ScrollView>
+			   </View> 
 	  )
 	}
 }
 
-// const mapStateToProps = (store) =>{
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 15,
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  title:{
+    flexDirection: 'column',
+    margin: 10
+  },
+  logo: {
+    width: 250,
+    height: 50,
+    margin: 2
+  }, 
+  image: {
+  	height: 300,
+  	width: width,
+  	padding: 5
+  }, 
+  profilePicture: {
+		margin: 5,
+		width: 30, 
+		height: 30,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 20
+	},
+	profileInfo: {
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	caption:{
+		fontSize: 14,
+		color: 'grey',
+		textAlign: 'center',
+		fontFamily: 'Verdana'
+	},
+	likes: {
+		padding: 3,
+		color: 'grey',
+		fontSize: 14,
+		fontFamily: 'Verdana',
+		fontWeight: 'bold'
+	}, 
+	userCommentName: {
+		fontWeight: 'bold'
+	},
+	userComment:{
+		fontWeight: 'normal',
+		fontSize: 13,
+		fontFamily: 'Verdana',
+	}
+
+})
+const mapStateToProps = (store) =>{
  
-//   return {
-//     profileInfo: store
-//   }
-// }
-
-
-export default Page
+  return {
+    pageInfo: store
+  }
+}
+export default connect(mapStateToProps)(Page)
