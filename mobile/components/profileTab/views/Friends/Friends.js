b5ffc454ee8fd3a966a08e390ef474addefb0ffa
Angie Tang <atang167@gmail.com>
2017-10-31 10:28:03 -0700

    Use new routes for friends list

--- mobile/components/profileTab/views/Friends/Friends.js
+++ mobile/components/profileTab/views/Friends/Friends.js
@@ -1,36 +1,65 @@
 import React, { Component } from 'react';
-import { View, Button, TouchableWithoutFeedback } from 'react-native'
+import { View, Button, TouchableWithoutFeedback, Text, Image } from 'react-native'
 import { connect } from 'react-redux'
+import { bindActionCreators} from 'redux'
 import axios from 'axios'
 
+import * as friendActions from '../../../../actions/friendActions'
+
 import { SearchBar } from 'react-native-elements'
-import FriendsEntry from './FriendsEntry'
+
+//import FriendProfile from './FriendProfile'
 
 class Friends extends Component {
   constructor(props) {
     super(props); 
+    // this.state = {
+    //   friends: [{id: 1, img: "", name: "Angie"}, {id: 2, img: "", name: "Jordan"}  ], 
+    //   results: [{id: 2, img: "", name: "Jeff"}, {id: 4, img: "", name: "Daniel"}],
+    //   input: ''
+    // }
     this.state = {
-      friends: [{id: 1, img: "", name: "Angie"}, {id: 2, img: "", name: "Jordan"}  ], 
-      results: [{id: 2, img: "", name: "Jeff"}, {id: 4, img: "", name: "Daniel"}],
+      results: [],
       input: ''
     }
   }
+
+  componentDidMount() {
+    // axios.get('api/friends/' + this.props.userId)
+    // .then(({ data }) => {
+    //   data.map(data => {
+    //     if (data.user_id !== this.props.userId) {
+    //       this.setState({ friends: this.state.friends.concat(data.user_id)})
+    //     }
+    //     if (data.friend_id !== this.props.userId) {
+    //       this.setState({ friends: this.state.friends.concat(data.friend_id)})
+    //     }
+    //   })
+    // })
+    // .catch(err => {
+    //   console.log('failed to retrieve friends', err);
+    // })
+    // console.log('userId', this.screenProps);
+    // this.props.actions.getFriends(this.screenProps);
+    this.props.actions.getFriends(1);
+  }
   
-  searchFriends(firstName, lastName) {
-    axios.get('api/search/' + firstName + '/' + lastName)
-    .then(({ data }) => {
-      this.setState({results: data})
-    })
-    .catch(err => {
-      console.log('failed to search friends', err);
-    })
+  searchUsers(name) {
+    // this.setState({
+    //   results: this.props.friends.filter(friend => {
+    //     if (friend.name.includes(name)) {
+    //       return friend; 
+    //     }
+    //   })
+    // });
+    this.state.results.filter()
   }
 
   sendRequest(friendId) {
-    axios.post('/api/addFriend', {
+    axios.post('http://localhost:5000/api/add_friend', {
       friendId: friendId,
-      userId: this.props.userId,
-      type: 'pending'
+      // userId: this.screenProps,
+      userId: 1
     })
     .then(({ data }) => {
       console.log('Success sending request', data);
@@ -40,33 +69,13 @@ class Friends extends Component {
     })
   }
 
-  getRequests() {
-    
-  }
-
   clearSearch() {
     this.setState({results: []});
   }
 
-  componentDidMount() {
-    axios.get('api/friends/' + this.props.userId)
-    .then(({ data }) => {
-      data.map(data => {
-        if (data.user_id !== this.props.userId) {
-          this.setState({ friends: this.state.friends.concat(data.user_id)})
-        }
-        if (data.friend_id !== this.props.userId) {
-          this.setState({ friends: this.state.friends.concat(data.friend_id)})
-        }
-      })
-    })
-    .catch(err => {
-      console.log('failed to retrieve friends', err);
-    })
-  }
-
   render() {
     const {navigate} = this.props.navigation; 
+    console.log('props passed to friends', this.props);
     if (this.state.results.length) {
       return (
         <View>
@@ -91,17 +100,29 @@ class Friends extends Component {
             title="Go back to profile"
           /> 
           {this.state.results.map(result => {
-            return ( 
-              <TouchableWithoutFeedback>
-                <FriendsEntry 
-                  sendRequest={this.sendRequest.bind(this)} 
-                  friends={this.state.friends} 
-                  id={result.id} 
-                  img={result.profile_img_url} 
-                  name={result.name}
-                /> 
-              </TouchableWithoutFeedback>
-            )
+            for (let i = 0; i < this.props.friends.length; i++) {
+              if (result.id === friends[i].id) {
+                return (
+                  <TouchableWithoutFeedback onPress={() => navgiate('FriendProfile', {userId: result.id})}>
+                    <Text>{result.name}</Text> 
+                    <Image source={result.img} />
+                  </TouchableWithoutFeedback>
+                )
+              } else {
+                return (
+                  <View>
+                    <TouchableWithoutFeedback onPress={() => navgiate('FriendProfile', {userId: result.id})}>
+                      <Text>{result.name}</Text> 
+                      <Image source={result.img} />
+                    </TouchableWithoutFeedback>
+                    <Button
+                      title="Add friend"
+                      onPress={() => {this.sendRequest(result.id)}}
+                    ></Button>
+                  </View> 
+                )
+              }
+            }
           })}
       </View> 
       )
@@ -110,11 +131,9 @@ class Friends extends Component {
         <View>
           <SearchBar 
             placeholder="Search"
-            searchFriends={this.searchFriends.bind(this)} 
             onChangeText={(text) => {this.setState({input: text})}}
             onSubmitEditing={() => {
-              this.searchFriends(this.state.input.split(' ')[0], this.state.input.split(' ')[1]);
-              clear();
+              this.searchUsers(this.state.input);
             }}
             //add icon to clear searches
           />
@@ -126,13 +145,13 @@ class Friends extends Component {
             onPress={() => navigate('FriendRequests')}
             title="Go to friend requests"
           /> 
-          {this.state.friends.map(friend => {
+          {this.props.friends.map(friend => {
             return (
-              <TouchableWithoutFeedback>
-                <FriendsEntry 
-                  img={friend.profile_img_url} 
-                  name={friend.name}
-                /> 
+              <TouchableWithoutFeedback onPress={() => navgiate('FriendProfile', {userId: friend.id})}>
+                  <View>
+                    <Text>{friend.name}</Text>
+                    <Image source={friend.profile_img_url}/> 
+                  </View> 
               </TouchableWithoutFeedback>
             ) 
           })} 
@@ -144,8 +163,14 @@ class Friends extends Component {
 
 const mapStateToProps = (store) => {
   return {
-    userId: store.Auth.userId
+    friends: store.Friends.friends
    }
  }
 
-export default connect(mapStateToProps)(Friends);
+ const friendDispatch = (dispatch) => {
+   return {
+     actions: bindActionCreators(friendActions, dispatch),
+   }
+ }
+
+export default connect(mapStateToProps, friendDispatch)(Friends);
