c96b9924e70784b514ff2f58e001522fdd47f87b
Angie Tang <atang167@gmail.com>
2017-10-31 10:30:49 -0700

    Add new screens friends

--- mobile/components/profileTab/navigationConfig.js
+++ mobile/components/profileTab/navigationConfig.js
@@ -6,6 +6,8 @@ import Chat from './views/Messages/Chat'
 import UserMap from './views/map.js'
 import PicturesFromMarker from './views/picturesFromMarker'
 import FriendRequests from './views/Friends/FriendRequests'
+import NewChat from './views/Messages/NewChat'
+//import FriendProfile from './views/Friends/FriendProfile'
 
 const routeConfig = {
   Profile: { screen: Profile},
@@ -13,8 +15,11 @@ const routeConfig = {
   Messages: { screen: Messages },
   Chat: { screen: Chat },
   UserMap: { screen: UserMap },
-  PicturesFromMarker: { screen: PicturesFromMarker},
+  PicturesFromMarker: { screen: PicturesFromMarker },
   //ARView: {screen: ARView},
+  NewChat: { screen: NewChat },
+  FriendRequests: { screen: FriendRequests }
+  //FriendProfile: { screen: FriendProfile }
 }
 // going to disable the header for now
 const stackNavigatorConfig = {
