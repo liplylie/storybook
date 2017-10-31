dcb281d007608497d729ad910b3e07b67cb5f2dc
Angie Tang <atang167@gmail.com>
2017-10-31 10:31:48 -0700

    Make message entry dumb component

--- mobile/components/profileTab/views/Messages/MessageEntry.js
+++ mobile/components/profileTab/views/Messages/MessageEntry.js
@@ -4,26 +4,29 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as chatActions from '../../../../actions/chatActions'
 
-// const MessageEntry = (props) => {
-//   const {navigate} = this.props.navigate;
-//   return (
-//     <TouchableWithoutFeedback onPress={()=> navigate('Chat')}>
-//       <Text>{this.props.friend}</Text>
-//       <Text>{this.props.message}</Text>
-//       <Image source={this.props.img}/>
-//     </TouchableWithoutFeedback> 
-//   )
-// }
+const MessageEntry = (props) => {
+  const {navigate} = this.props.navigate;
+  return (
+    <TouchableWithoutFeedback onPress={() => {
+      this.props.actions.enterRoom(this.props.roomId);
+      navigate('Chat');
+    }}>
+      <Text>{this.props.friend}</Text>
+      <Text>{this.props.message}</Text>
+      <Image source={this.props.img}/>
+    </TouchableWithoutFeedback> 
+  )
+}
 
-class MessageEntry extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      img: ''
-    }
-  }
-  componentDidMount() {
+// class MessageEntry extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: '',
+//       img: ''
+//     }
+//   }
+  // componentDidMount() {
     // axios.get('api/friend/' + this.props.friendId)
     // .then(({ data }) => {
     //   this.setState({
@@ -34,26 +37,26 @@ class MessageEntry extends Component {
     // .catch(err => {
     //   console.log('Unable to get friend info', err);
     // })
-  }
+  // }
 
-  render() {
-    const {navigate} = this.props.navigation;
-    return (
-      <View>
-        <TouchableWithoutFeedback 
-          onPress={() => {
-            this.props.actions.enterRoom(this.props.roomId)
-            navigate('Chat');
-          }}
-          >
-          <Text>{this.props.friend}</Text>
-        </TouchableWithoutFeedback> 
-        <Text>{this.props.message}</Text>
-        <Image source={this.props.img}/>
-      </View>
-    )
-  }
-}
+//   render() {
+//     const {navigate} = this.props.navigation;
+//     return (
+//       <View>
+//         <TouchableWithoutFeedback 
+//           onPress={() => {
+            
+//             navigate('Chat');
+//           }}
+//           >
+//           <Text>{this.props.friend}</Text>
+//         </TouchableWithoutFeedback> 
+//         <Text>{this.props.message}</Text>
+//         <Image source={this.props.img}/>
+//       </View>
+//     )
+//   }
+// }
 
 
 const chatDispatch = (dispatch) => {
