d0762ab66031c8e7515eea0e1cd65073bae7935a
Angie Tang <atang167@gmail.com>
2017-10-31 10:30:04 -0700

    Add new socket channels

--- react_server/server.js
+++ react_server/server.js
@@ -31,31 +31,28 @@ app.get('/*', function (req, res) {
 })
 
 io.on('connection', socket => {
+  // console.log('A client just joined on', socket.id);
   socket.on('message', message => {
     console.log('server received message', message);
     console.log('this is the message room', message.roomId);
-    socket.broadcast.to(message.roomId).emit('message', {
-      message: message.message,
-      sender: message.sender,
-      roomId: message.roomId
-    })
+    socket.broadcast.emit(message.roomId, message)
     // db.Messages.create({
       //message: message.message,
       //sender: message.sender,
       //room_id: parseInt(message.roomId),
     //});
   })
-  socket.on('subscribe', roomId => {
-    socket.join(roomId);
-    console.log('joining room', roomId);
+  // socket.on('subscribe', roomId => {
+  //   socket.join(roomId);
+  //   console.log('joining room', roomId);
     // let messages = db.Messages.findAll({
     //   where: {room_id: parseInt(roomId)},
     //   order: [[ 'createdAt', 'DESC' ]]
     // });
     // socket.emit('message', messages);
-  })
-  console.log('user connected', io.engine.clientsCount);
-})
+//   })
+//   console.log('user connected', io.engine.clientsCount);
+// })
 
 io.on('disconnect', socket => {
   console.log('user disconnected', io.engine.clientsCount);
