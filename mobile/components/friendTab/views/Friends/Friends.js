7d8d2de48a148e3f61414f1afe710f86ebe8f58b
Angie Tang <atang167@gmail.com>
2017-10-31 20:02:42 -0700

    Remove user search from friends view

--- mobile/components/friendTab/views/Friends/Friends.js
+++ mobile/components/friendTab/views/Friends/Friends.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, TouchableWithoutFeedback, Text, Image, StyleSheet } from 'react-native'
+import { View, Button, TouchableWithoutFeedback, Text, Image, ScrollView, StyleSheet } from 'react-native'
 import { SearchBar, Card, Icon } from 'react-native-elements'
 
 import { connect } from 'react-redux'
@@ -24,9 +24,6 @@ const styles = StyleSheet.create({
 	}
 })
 
-
-//import FriendProfile from './FriendProfile'
-
 class Friends extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: `Friends`,
@@ -41,117 +38,19 @@ class Friends extends Component {
       results: [],
       input: ''
     }
-    // this.state = {
-    //   results: [],
-    //   input: ''
-    // }
   }
 
   componentDidMount() {
-    // axios.get('api/friends/' + this.props.userId)
-    // .then(({ data }) => {
-    //   data.map(data => {
-    //     if (data.user_id !== this.props.userId) {
-    //       this.setState({ friends: this.state.friends.concat(data.user_id)})
-    //     }
-    //     if (data.friend_id !== this.props.userId) {
-    //       this.setState({ friends: this.state.friends.concat(data.friend_id)})
-    //     }
-    //   })
-    // })
-    // .catch(err => {
-    //   console.log('failed to retrieve friends', err);
-    // })
-    // console.log('userId', this.screenProps);
     // this.props.actions.getFriends(this.screenProps);
     // this.props.actions.getFriends(1);
   }
   
-  searchUsers(name) {
-    // this.setState({
-    //   results: this.props.friends.filter(friend => {
-    //     if (friend.name.includes(name)) {
-    //       return friend; 
-    //     }
-    //   })
-    // });
-    
-  }
-
-  sendRequest(friendId) {
-    axios.post('http://localhost:5000/api/add_friend', {
-      friendId: friendId,
-      // userId: this.screenProps,
-      userId: 1
-    })
-    .then(({ data }) => {
-      console.log('Success sending request', data);
-    })
-    .catch(err => {
-      console.log('Request failed', err);
-    })
-  }
-
-  clearSearch() {
-    this.setState({results: []});
-  }
 
   render() {
     const {navigate} = this.props.navigation; 
     console.log('props passed to friends', this.props);
-    if (this.state.results.length) {
       return (
-        <View>
-          <SearchBar
-            placeholder="Search"
-            onChangeText={(text) => {this.setState({input: text})}}
-            /* onSubmitEditing={() => {
-              this.searchFriends(this.state.input.split(' ')[0], this.state.input.split(' ')[1]);
-            }} */
-            //add icon to clear searches
-          /> 
-          {this.state.results.map(result => {
-            // for (let i = 0; i < this.props.friends.length; i++) {
-            //   if (result.id === friends[i].id) {
-                return (
-                  <Card>
-                  <TouchableWithoutFeedback onPress={() => navigate('FriendProfile', {userId: result.id, type: "friend"})}>
-                    <Image style={styles.image} resizeMode="cover" source={{uri: result.img}}/>
-                  </TouchableWithoutFeedback >
-                  <TouchableWithoutFeedback onPress={() => navigate('FriendProfile', {userId: result.id, type: "friend"})}>
-                    <Text style={styles.text}>{result.name}</Text>
-                  </TouchableWithoutFeedback> 
-                  </Card> 
-                )
-              // } else {
-              //   return (
-              //     <View>
-              //       <TouchableWithoutFeedback onPress={() => navgiate('FriendProfile', {userId: result.id, type: "result"})}>
-              //         <Text>{result.name}</Text> 
-              //         <Image source={result.img} />
-              //       </TouchableWithoutFeedback>
-              //       <Button
-              //         title="Add friend"
-              //         onPress={() => {this.sendRequest(result.id)}}
-              //       ></Button>
-              //     </View> 
-              //   )
-              // }
-            // }
-          })}
-      </View> 
-      )
-    } else {
-      return (
-        <View>
-          <SearchBar 
-            placeholder="Search"
-            onChangeText={(text) => {this.setState({input: text})}}
-            onSubmitEditing={() => {
-              this.searchUsers(this.state.input);
-            }}
-            //add icon to clear searches
-          />
+        <ScrollView>
           {this.props.friends.map(friend => {
             return (
               <Card>
@@ -164,9 +63,8 @@ class Friends extends Component {
               </Card>
             ) 
           })} 
-        </View> 
+        </ScrollView> 
       )
-    }
   }
 }
 
