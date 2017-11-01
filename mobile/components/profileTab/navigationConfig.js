a180bc118da053348eb92169b4d3d733b0f0822a
Jordan Daniels <decentpianist@gmail.com>
2017-10-31 20:00:55 -0700

    prepare for pull request

--- mobile/components/profileTab/navigationConfig.js
+++ mobile/components/profileTab/navigationConfig.js
@@ -3,7 +3,7 @@ import Profile from './views/Profile'
 import UserMap from './views/map.js'
 import PicturesFromMarker from './views/picturesFromMarker'
 import FriendRequests from './views/FriendRequests'
-import NewChat from './views/Messages/NewChat'
+//import NewChat from './views/Messages/NewChat'
 import ARView from '../homeTab/views/AR'
 //import FriendProfile from './views/Friends/FriendProfile'
 
@@ -12,7 +12,7 @@ const routeConfig = {
   UserMap: { screen: UserMap },
   PicturesFromMarker: { screen: PicturesFromMarker },
   ARView: {screen: ARView},
-  NewChat: { screen: NewChat },
+  //NewChat: { screen: NewChat },
   FriendRequests: { screen: FriendRequests }
 }
 
