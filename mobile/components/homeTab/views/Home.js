c06f154f7ba98d848773fad29350232fc5b5c389
Angie Tang <atang167@gmail.com>
2017-10-31 19:56:09 -0700

    Add user search bar to home

--- mobile/components/homeTab/views/Home.js
+++ mobile/components/homeTab/views/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { SearchBar } from 'react-native-elements'
 import {
   Platform,
   StyleSheet,
@@ -15,28 +16,103 @@ import Spinner from 'react-native-spinkit';
 //import Login from '../../auth/Login.js'
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      results: []
+    }
+  }
   static navigationOptions = {
     header: null
   }
-  render(){
-    if (this.props.profileInfo.userLocation){
+
+  searchUsers(input) {
+    //axios.get
+  }
+
+  render() {
+    const {navigate} = this.props.navigation;
+    if (this.state.results.length) {
+      return (
+        <View>
+          <SearchBar
+            placeholder="Search"
+            onChangeText={(text) => {
+              if (text === '') {
+                this.setState({results: []});
+              }
+              this.setState({input: text});
+            }}
+            onSubmitEditing={() => {
+              this.searchFriends(this.state.input);
+            }}
+            clearIcon={	{ color: '#86939e', name: 'clear' } }
+          /> 
+          <List>
+          {this.state.results.map((result) => {
+            for (let i = 0; this.props.friends.length; i++) {
+              if (result.id === friends[i].id) {
+                return (
+                  <ListItem 
+                    roundAvatar
+                    avatar={{uri: result.img}}
+                    title={result.name}
+                    onPress={() => {
+                      navigate('FriendProfile', {userId: result.id, type: 'friend'});
+                    }}>
+                  </ListItem>
+                )
+              } else {
+                return (
+                  <ListItem 
+                    roundAvatar
+                    avatar={{uri: result.img}}
+                    title={result.name}
+                    onPress={() => {
+                      navigate('FriendProfile', {userId: result.id, type: 'result'});
+                    }}>
+                  </ListItem>
+                )
+              }
+            }})
+          })}
+          </List>
+          }}
+        </View> 
+      )
+    } else if (this.props.profileInfo.userLocation) {
       console.log(this.props, 'home props')
       let location = this.props.profileInfo.userLocation;
       let navigation = this.props.navigation
       return (
           <View style={styles.container}>
-              <View style={styles.title}> 
-                <Image 
-                  style={styles.image}
-                  resizeMethod='resize'
-                  resizeMode='contain'
-                  source={require('../../../logo.jpg')} 
-                />
-              </View>
-              <View style={styles.collection}>
-                <Collection location={location} navigation={navigation}/>
-              </View>
-          </View>
+             <SearchBar
+                placeholder="Search"
+                onChangeText={(text) => {
+                  if (text === '') {
+                    this.setState({results: []});
+                  }
+                  this.setState({input: text});
+                }}
+                onSubmitEditing={() => {
+                  this.searchFriends(this.state.input);
+                }}
+                clearIcon={	{ color: '#86939e', name: 'clear' } }
+             //add icon to clear searches
+            /> 
+            <View style={styles.title}> 
+              <Image 
+                style={styles.image}
+                resizeMethod='resize'
+                resizeMode='contain'
+                source={require('../../../logo.jpg')} 
+              />
+            </View>
+            <View style={styles.collection}>
+              <Collection location={location} navigation={navigation}/>
+            </View>
+        </View>
       );
     } else {
       return (
@@ -80,9 +156,9 @@ const styles = StyleSheet.create({
 });
 
 const mapStateToProps = (store) =>{
- 
   return {
-    profileInfo: store.Profile
+    profileInfo: store.Profile,
+    friends: store.Chat.friends
   }
 }
 
