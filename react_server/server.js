9827982c794029b790868256c8baca8e62b52eed
Angie Tang <atang167@gmail.com>
2017-11-01 18:41:49 -0700

    Fix socket channel

--- react_server/server.js
+++ react_server/server.js
@@ -32,10 +32,13 @@ app.get('/*', function (req, res) {
 
 io.on('connection', socket => {
   // console.log('A client just joined on', socket.id);
-  socket.on('message', message => {
+  // socket.on('message', message => {
+  socket.on('message', (message) => {
     console.log('server received message', message);
-    console.log('this is the message room', message.roomId);
-    socket.broadcast.emit(message.roomId, message)
+    // console.log('this is the message room', message.roomId);
+    console.log('this is the message room', '1');
+    // socket.broadcast.emit(message.roomId, message)
+    socket.to('1').emit('message', message);
     // db.Messages.create({
       //message: message.message,
       //sender: message.sender,
@@ -51,11 +54,12 @@ io.on('connection', socket => {
     // });
     // socket.emit('message', messages);
 //   })
-//   console.log('user connected', io.engine.clientsCount);
+  console.log('user connected', io.engine.clientsCount);
 // })
 
-io.on('disconnect', socket => {
-  console.log('user disconnected', io.engine.clientsCount);
+  io.on('disconnect', socket => {
+    console.log('user disconnected', io.engine.clientsCount);
+  })
 })
 
 // User.sync()
