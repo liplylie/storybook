bee90afe49ed25f9f278bdfc2512ff8561fc2066
Angie Tang <atang167@gmail.com>
2017-11-01 18:40:47 -0700

    Add profile component

--- mobile/components/friendTab/views/Friends/FriendProfile.js
+++ mobile/components/friendTab/views/Friends/FriendProfile.js
@@ -76,3 +76,148 @@ const mapStateToProps = (store) => {
 //  }
 
 export default connect(mapStateToProps)(FriendProfile); 
+
+// import React from 'react';
+// import { View, Text, Image, StyleSheet, ScrollView, Dimensions, TouchableHighlight } from 'react-native';
+// import { connect } from 'react-redux';
+// import UserMap from './map.js';
+// import Login from '../../auth/Login'
+
+// const { width, height } =  Dimensions.get('window');
+// const Profile = ( props ) => {
+// 	console.log(props, 'profile props')
+// 	if (props.profileInfo.Auth.name){
+// 		let name = props.profileInfo.Auth.name;
+// 		let email = props.profileInfo.Auth.email;
+// 		let picture = props.profileInfo.Auth.picture;
+// 		let id = props.profileInfo.Auth.id;
+// 		let friendIcon = "https://cdn.iconscout.com/public/images/icon/premium/png-512/profile-group-friend-team-user-avatar-3d104201ce065c29-512x512.png"
+
+// 		const viewFriends = () => {
+//     	props.navigation.navigate("Friends")
+// 		}
+		
+// 		const viewRequests = () => {
+// 			props.navigation.navigate("FriendRequests")
+// 		}
+
+//   	const getFriends = () => {
+//   		// get all friends, count them all, return count
+//   		// put user id in get request params
+//   		//'/api/get_all_friends'
+//   	}
+
+// 		return (
+// 	    <ScrollView style={styles.profileContainer}>
+// 	    	<View style={{flex:.3, backgroundColor: 'white', marginTop:10, justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
+// 					<TouchableHighlight onPress={viewRequests} >
+// 			      <Image source={{uri:friendIcon}} style={styles.friendIcon}/> 
+// 			  	</TouchableHighlight>
+// 	    		<Text style={styles.email}>
+// 		      	{email}
+// 		      </Text>
+// 		      <TouchableHighlight onPress={viewFriends} >
+// 			      <Image source={{uri:friendIcon}} style={styles.friendIcon}/> 
+// 			    </TouchableHighlight>
+// 	    	</View>
+// 	    	<View style ={styles.profileDetails}>
+// 	    		<View style={{flexDirection: 'row', flex: 1}}>
+// 	    			<View style={{flexDirection: 'column', flex: 1}}>
+// 	    				<View style={{flexDirection: 'row', alignItems: 'center'}}>
+// 			    			<Image source={{uri:picture}} style={styles.profilePicture}/>
+// 			    			<View style={{paddingLeft: 150, justifyContent: 'space-around', flexDirection: 'row'}}>
+// 			    				<View style={{marginRight: 20}}>
+// 				    				<Text> Photos </Text>
+// 				    				<Text style={{textAlign: 'center'}}> 0 </Text>
+// 				    			</View>
+// 				    			<View>
+// 					    			<Text> Friends </Text>
+// 				    				<Text style={{textAlign: 'center'}}> 0 </Text>
+// 				    			</View>
+// 			    			</View>
+// 			    		</View>
+// 			    		<Text style={styles.profileName}> {name} </Text>
+// 			    	</View>
+// 		    		<View style={{ marginLeft: 180}}>
+			    		
+// 		      	</View>
+// 		    	</View>
+		      	
+// 		      	<View style={{margin: 5}}>
+// 		      		<Login />
+// 		      	</View>
+// 		    </View>
+
+
+
+
+
+
+// 	      <View style={styles.profileMap}>
+// 	      	<UserMap navigation={props.navigation}/>
+// 	      </View>
+// 	    </ScrollView> 
+// 	  )
+// 	}	else {
+// 		  return (
+// 		    <View>
+// 		    </View> 
+// 	  	)
+// 	}
+// }
+
+// const mapStateToProps = (store) =>{
+//   console.log(store, 'profile js state')
+ 
+//   return {
+//     profileInfo: store
+//   }
+// }
+
+// const styles = StyleSheet.create({
+// 	profileContainer: {
+// 		flex: 1,
+// 		flexDirection: 'row',
+// 		backgroundColor: 'white'
+// 	},
+// 	profileDetails: {
+// 		flex: 1,
+// 		backgroundColor: 'skyblue'
+// 	},
+// 	profilePicture: {
+// 		marginTop: 15,
+// 		marginLeft: 30,
+// 		marginBottom: 10,
+// 		width: 50, 
+// 		height: 50,
+// 		justifyContent: 'center',
+// 		alignItems: 'center',
+// 		borderRadius: 20
+// 	}, 
+// 	friendIcon: {
+// 		width: 40, 
+// 		height: 40,
+// 		borderRadius: 10,
+// 		marginLeft: 30,
+// 	},
+// 	email: {
+// 		paddingTop: 5,
+// 		fontSize: 14,
+// 		fontFamily: 'Verdana',
+// 		fontWeight: 'bold',
+// 		marginRight: 20,
+// 	},
+// 	profileName: {
+// 		fontSize: 14,
+// 		paddingBottom: 5,
+// 		fontFamily: 'Verdana'
+// 	},
+// 	profileMap: {
+// 		flex: 3,
+// 		flexDirection: 'column',
+// 		width: width,
+// 	}
+// })
+
+
+// export default connect(mapStateToProps, null)(Profile);
--- mobile/components/friendTab/views/Friends/Friends.js
+++ mobile/components/friendTab/views/Friends/Friends.js
@@ -11,23 +11,29 @@ import * as friendActions from '../../../../actions/friendActions'
 
 const styles = StyleSheet.create({
 	image: {
-			marginTop: .5,
-			marginLeft: 5, 
-			marginRight: 5, 
-			marginBottom: 5,
-			width: 110, 
-			height: 110,
-			borderRadius: 10
-		},
+    marginTop: .5,
+    marginLeft: 5, 
+    marginRight: 5, 
+    marginBottom: 5,
+    width: 110, 
+    height: 110,
+    borderRadius: 10
+	},
 	text: {
 		textAlign: 'center'
-	}
+  },
+  header: {
+    paddingRight: 10
+  },
+  component: {
+    backgroundColor: 'white'
+  }
 })
 
 class Friends extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: `Friends`,
-    headerRight: <Icon name='message' type='entypo' style={{marginRight: 10}} onPress={() => navigation.navigate('Messages')} />  
+    headerRight: <Icon name='message' type='entypo' onPress={() => navigation.navigate('Messages')} style={styles.header} />  
   });
 
   constructor(props) {
@@ -42,7 +48,7 @@ class Friends extends Component {
 
   componentDidMount() {
     // this.props.actions.getFriends(this.screenProps);
-    // this.props.actions.getFriends(1);
+    this.props.actions.getFriends(1);
   }
   
 
@@ -50,7 +56,7 @@ class Friends extends Component {
     const {navigate} = this.props.navigation; 
     console.log('props passed to friends', this.props);
       return (
-        <ScrollView>
+        <ScrollView style={styles.component}>
           {this.props.friends.map(friend => {
             return (
               <Card>
@@ -81,3 +87,170 @@ const mapStateToProps = (store) => {
  }
 
 export default connect(mapStateToProps, friendDispatch)(Friends);
+
+// import React, { Component } from 'react';
+// import { SearchBar } from 'react-native-elements'
+// import {
+//   Platform,
+//   StyleSheet,
+//   Text,
+//   View,
+//   Image,
+//   ScrollView
+// } from 'react-native';
+// import Collection from './Collection';
+// import { connect } from 'react-redux';
+// import Spinner from 'react-native-spinkit';
+
+
+//import Login from '../../auth/Login.js'
+
+// class Home extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       input: '',
+//       results: []
+//     }
+//   }
+//   static navigationOptions = {
+//     header: null
+//   }
+
+//   searchUsers(input) {
+//     //axios.get
+//   }
+
+//   render() {
+//     const {navigate} = this.props.navigation;
+//     if (this.state.results.length) {
+//       return (
+//         <View>
+//           <SearchBar
+//             placeholder="Search"
+//             onChangeText={(text) => {
+//               if (text === '') {
+//                 this.setState({results: []});
+//               }
+//               this.setState({input: text});
+//             }}
+//             onSubmitEditing={() => {
+//               this.searchFriends(this.state.input);
+//             }}
+//             clearIcon={	{ color: '#86939e', name: 'clear' } }
+//           /> 
+//           <List>
+//           {this.state.results.map((result) => {
+//             for (let i = 0; this.props.friends.length; i++) {
+//               if (result.id === friends[i].id) {
+//                 return (
+//                   <ListItem 
+//                     roundAvatar
+//                     avatar={{uri: result.img}}
+//                     title={result.name}
+//                     onPress={() => {
+//                       navigate('FriendProfile', {userId: result.id, type: 'friend'});
+//                     }}>
+//                   </ListItem>
+//                 )
+//               } else {
+//                 return (
+//                   <ListItem 
+//                     roundAvatar
+//                     avatar={{uri: result.img}}
+//                     title={result.name}
+//                     onPress={() => {
+//                       navigate('FriendProfile', {userId: result.id, type: 'result'});
+//                     }}>
+//                   </ListItem>
+//                 )
+//               }
+//             }})
+//           })}
+//           </List>
+//           }}
+//         </View> 
+//       )
+//     } else if (this.props.profileInfo.userLocation) {
+//       console.log(this.props, 'home props')
+//       let location = this.props.profileInfo.userLocation;
+//       let navigation = this.props.navigation
+//       return (
+//           <View style={styles.container}>
+//              <SearchBar
+//                 placeholder="Search"
+//                 onChangeText={(text) => {
+//                   if (text === '') {
+//                     this.setState({results: []});
+//                   }
+//                   this.setState({input: text});
+//                 }}
+//                 onSubmitEditing={() => {
+//                   this.searchFriends(this.state.input);
+//                 }}
+//                 clearIcon={	{ color: '#86939e', name: 'clear' } }
+//              //add icon to clear searches
+//             /> 
+//             <View style={styles.title}> 
+//               <Image 
+//                 style={styles.image}
+//                 resizeMethod='resize'
+//                 resizeMode='contain'
+//                 source={require('../../../logo.jpg')} 
+//               />
+//             </View>
+//             <View style={styles.collection}>
+//               <Collection location={location} navigation={navigation}/>
+//             </View>
+//         </View>
+//       );
+//     } else {
+//       return (
+//         <View style={styles.spinnerContainer}>
+//           <Spinner type='FadingCircle' style={styles.spinner}/>
+//         </View>
+//       )
+//     }
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginTop: 15,
+//     alignItems: 'center',
+//     backgroundColor: 'white'
+//   },
+//   title:{
+//     flexDirection: 'column',
+//     margin: 10
+//   },
+//   image: {
+//     width: 250,
+//     height: 50,
+//     margin: 2
+//   },
+//   collection: {
+//     backgroundColor: 'skyblue',
+//     flex:1,
+//   },
+//   spinnerContainer:{
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   spinner:{
+//     justifyContent: 'center',
+//     alignItems: 'center'
+//   }
+// });
+
+// const mapStateToProps = (store) =>{
+//   return {
+//     profileInfo: store.Profile,
+//     friends: store.Chat.friends
+//   }
+// }
+
+
+// export default connect(mapStateToProps, null)(Home)
