f404a33b4b56b355b40a01465b5af5e18794942c
Jordan Daniels <decentpianist@gmail.com>
2017-11-03 14:41:28 -0700

    prepare for merge

--- mobile/components/friendTab/views/Messages/Chat.js
+++ mobile/components/friendTab/views/Messages/Chat.js
@@ -34,6 +34,8 @@ class Chat extends Component {
     this.state = {
       messages: [],
     };
+    this.onSend = this.onSend.bind(this)
+
   }
   
   componentDidMount() {
@@ -53,8 +55,9 @@ class Chat extends Component {
 
     this.socket = io(key.react_server) 
       // roomId is just whatever you pass down to use as the room
-      // query: `roomId=${roomId}`
+      // query: `roomId=${roomId}`]
     this.socket.on('message', (message) => {
+      console.log(message, 'message from user')
         this.setState((previousState) => ({
         messages: GiftedChat.append(previousState.messages, message),
       }));
@@ -120,6 +123,7 @@ class Chat extends Component {
 
   onSend(message) {
       // Any additional custom parameters are passed through
+      console.log(message,'chat message')
     this.socket.emit('message', message);
     this.setState((previousState) => ({
       messages: GiftedChat.append(previousState.messages, message),
